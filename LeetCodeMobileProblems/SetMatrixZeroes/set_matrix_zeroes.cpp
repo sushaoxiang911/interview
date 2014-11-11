50b6c387c7c88582a55eb8e2e8daa5d7674216eb
sushaoxiang911 <ssx@umich.edu>
2014-11-10 19:07:47 -0800

    set zeros

--- LeetCodeMobileProblems/SetMatrixZeroes/set_matrix_zeroes.cpp
+++ LeetCodeMobileProblems/SetMatrixZeroes/set_matrix_zeroes.cpp
@@ -40,8 +40,18 @@ void set_zeroes(vector<vector<int> > &matrix) {
         }
     }
 }
-/*
-// constant space is to every level, every level check
+
+// to set the whole row and col zeros
+// we can use the matrix itself to keep track of the rows and cols that we are 
+// going to set
+//
+// Thing to be considered is that we use the first row as the row record vector
+// and the first col as the col record vector
+// we have to know if we should set first row and first col to zero
+//
+// So that needs to use to flag to remember before we traverse the matrix
+
+
 void set_zeroes_2(vector<vector<int> > &matrix) {
     if (matrix.empty())
         return;
@@ -49,54 +59,66 @@ void set_zeroes_2(vector<vector<int> > &matrix) {
         return;
     int row_size = matrix.size();
     int col_size = matrix[0].size();
-    
-    int row = 1;
-    int col = 1;
 
-
-    while (row < row_size && col < col_size) {
-        for (int i = 0; i < row_size; ++i) {
-            for (int j = 0; j < col_size; ++j)
-                cout << matrix[i][j] << " ";
-            cout << endl;
+    bool rflag = false;
+    bool cflag = false;
+    for (int i = 0; i < row_size; ++i) {
+        if (matrix[i][0] == 0) {
+            cflag = true;
+            break;
         }
-        cout << endl;
-
-
-        // check previous level
-        for (int i = row - 1; i < row_size; ++i) {
-            if (matrix[i][col-1] == 0) 
-                matrix[i][col] = 0;
-        }
-        for (int j = col - 1; j < col_size; ++j) {
-            if (matrix[row-1][j] == 0)
-                matrix[row][j] = 0;
-        }
-        
-        for (int i = row; i < row_size; ++i) {
-            if (matrix[i][col] == 0) {
-                for (int j = 0; j < col; ++j)
-                    matrix[i][j] = 0;
-            }
-        }
-        for (int j = col; j < col_size; ++j) {
-            if (matrix[row][j] == 0) {
-                for (int i = 0; i < row; ++i)
-                    matrix[i][j] = 0;
-            }
-        }
-        row++;
-        col++;
     }
-     
+
+    for (int j = 0; j < col_size; ++j) {
+        if (matrix[0][j] == 0) {
+            rflag = true;
+            break;
+        }
+    }
+
+    for (int i = 1; i < row_size; ++i) {
+        for (int j = 1; j < col_size; ++j) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i < row_size; ++i) {
+        if (matrix[i][0] == 0) {
+            for (int j = 0; j < col_size; ++j) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    for (int j = 1; j < col_size; ++j) {
+        if (matrix[0][j] == 0) {
+            for (int i = 0; i < row_size; ++i) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (rflag) {
+        for (int j = 0; j < col_size; ++j) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (cflag) {
+        for (int i = 0; i < row_size; ++i) {
+            matrix[i][0] = 0;
+        }
+    }
 }
 
-*/
 int main() {
     vector<vector<int> > matrix {   {0, 3, 0, 1}, 
                                     {3, 5, 3, 2}, 
                                     {0, 1, 3, 4}};
-    set_zeroes(matrix);
+    set_zeroes_2(matrix);
     cout << "result: " << endl;
     for (int i = 0; i < matrix.size(); ++i) {
         for (int j = 0; j < matrix[i].size(); ++j)
