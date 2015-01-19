e7ee041e33898ee664f6c431a92dc9e359d90bfe
sushaoxiang911 <sushaoxiang911@SuShaoxiangs-MacBook-Pro.local>
2015-01-18 23:54:26 -0800

    better method

--- LeetCodeMobileProblems/ConvertSortedListToBinarySearchTree/convert_sorted_list_to_binary_search_tree.cpp
+++ LeetCodeMobileProblems/ConvertSortedListToBinarySearchTree/convert_sorted_list_to_binary_search_tree.cpp
@@ -14,7 +14,8 @@ struct tree_node {
 };
 
 // find the mid value can be done by two pointer
-
+// this takes nlogn time complexity since every time we have to go through all the nodes to find mid
+// and then construct it 
 tree_node* sorted_list_to_binary_search_tree (list_node* head) {
     if (head == NULL)
         return NULL;
@@ -45,3 +46,30 @@ tree_node* sorted_list_to_binary_search_tree (list_node* head) {
     return root;
         
 }
+
+// this takes n, with the help of list_node pointer reference, 
+// we go next of current every time so we do a depth first construction
+// and we just go through all the nodes once. 
+tree_node* sorted_list_to_binary_tree (list_node* head) {
+}
+
+
+tree_node* sorted_list_to_binary_tree_help(list_node* &current, int start, int end) {
+    if (start > end)
+        return NULL;
+    int mid = (start + end) / 2;
+    
+    tree_node* left = sorted_list_to_binary_tree(current, start, mid - 1);
+
+    tree_node* root = new tree_node;
+    root -> val = current -> val;
+    current = current -> next;
+
+    tree_node* right = sorted_list_to_binary_tree(current, mid + 1, end);
+
+    root -> left = left;
+    root -> right = right;
+
+    return root;
+}
+
